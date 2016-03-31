8e2cd47a45488299dfa419d4e664bd70715794eb
大盗乔三 <289880020@qq.com>
2016-03-31 09:32:44 +0800

    扩展移动端三个事件

--- js/event.js
+++ js/event.js
@@ -1,24 +1,96 @@
+/*移动端事件扩展
+ * 2016-3-30
+ * */
 (function (qc) {
-    /*事件扩展*/
+    /*点击事件*/
     qc.extendEvent({
         name: "tap",
-        touchstart: function (run, event) {
-            event.preventDefault();//修复touchend不执行bug
+        touchstart: function (run, event, element) {
+            this.y = event.touches[0].pageY;
+            this.x = event.touches[0].pageX;
+            element.className += " active";
+            this.run = true;
         },
-        touchend: function (run, event) {
-            run();
+        touchmove: function (run, event, element) {
+            if (this.run) {
+                if (Math.abs(event.touches[0].pageY - this.y) > 12 || Math.abs(event.touches[0].pageX - this.x) > 12) {
+                    this.run = false;
+                    element.className = element.className.replace(" active", "");
+                } else {
+                    event.preventDefault();//修复touchend不执行bug\
+                }
+            }
+        },
+        touchend: function (run, event, element) {
+            element.className = element.className.replace(" active", "");
+            if (this.run) {
+                run();
+            }
+        },
+        touchcancel: function (run, event, element) {
+            element.className = element.className.replace(" active", "");
         }
-    })
+    });
+    /*长按事件
+     * 说明:因为长按时间要屏蔽掉系统的默认事件，
+     * 所以在需要系统默认事件(比如滚动)的时候，不能使用长按时间
+     * */
     qc.extendEvent({
         name: "longtap",
-        touchstart: function (run, event) {
+        touchstart: function (run, event, element) {
             event.preventDefault();//修复touchend不执行bug
+            element.className += " active";
             this.timer = setTimeout(function () {
                 run();
-            }, 500);
+            }, 750)
         },
-        touchend: function (run, event) {
-            clearTimeout(this.timer);
+        touchend: function (run, event, element) {
+            element.className = element.className.replace(" active", "");
+            clearTimeout(run);
+        },
+        touchcancel: function (run, event, element) {
+            element.className = element.className.replace(" active", "");
+            clearTimeout(run);
         }
-    })
+    });
+    /*滑动事件
+     * 只有左滑(action=left)右滑(action=right)和点击(action=tap)
+     * 和点击
+     * */
+    qc.extendEvent({
+        name: "tapswipetd",
+        touchstart: function (run, event, element) {
+            this.y = event.touches[0].pageY;
+            this.x = event.touches[0].pageX;
+            this.endX = event.touches[0].pageX;
+            element.className += " active";
+            this.run = true;
+        },
+        touchmove: function (run, event, element) {
+            if (this.run) {
+                if (Math.abs(event.touches[0].pageY - this.y) <= 12) {
+                    event.preventDefault();//修复touchend不执行bug
+                    this.endX = event.touches[0].pageX;
+                } else {
+                    this.run = false;
+                    element.className = element.className.replace(" active", "");
+                }
+            }
+        },
+        touchend: function (run, event, element) {
+            element.className = element.className.replace(" active", "");
+            if (this.run) {
+                if (this.endX - this.x > 12) {
+                    run("right");
+                } else if (this.endX - this.x < -12) {
+                    run("left");
+                } else {
+                    run("tap");
+                }
+            }
+        },
+        touchcancel: function (run, event, element) {
+            element.className = element.className.replace(" active", "");
+        }
+    });
 })(qc)
