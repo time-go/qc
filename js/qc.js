5898bb4f09a6b2fded2c988261a20bfb5caee449
大盗乔三 <289880020@qq.com>
2016-04-01 09:15:08 +0800

    优化事件模块 上传动画模块

--- js/qc.js
+++ js/qc.js
@@ -1697,48 +1697,73 @@
                 var newDom = document.querySelector("[" + PREFIX + "-view='" + _vm + "']");
                 newDom.removeAttribute(PREFIX + "-view");
                 newDom.setAttribute(PREFIX + "-id", _vm);
+                var isIn = function (dom) {
+                    var _flag;
+                    try {
+                        _flag = dom.getAttribute(PREFIX + "-id");
+                        if (_flag !== undefined && _flag !== null) {
+                            return false;
+                        } else {
+                            if (dom.parentNode !== undefined && dom.parentNode !== null) {
+                                return isIn(dom.parentNode)
+                            } else {
+                                return true;
+                            }
+                        }
+                    } catch (e) {
+                        return true;
+                    }
+                    return true;
+                }
+                var isTemp = true;
+                if (newDom.parentNode !== undefined && dom.parentNode !== null) {
+                    isTemp = isIn(dom.parentNode);
+                }
+                if (!isTemp) {//如果在容器内部 就不比绑定事件了
+                    return;
+                }
                 newDom.onclick = function (e) {
                     var event = window.event || e;
                     event.target = event.target ? event.target : event.srcElement;//兼容IE
-                    bindEvent(event.target, _vm, "click", event);
+                    bindEvent(this, event.target, _vm, "click", event);
                 }
                 newDom.ondblclick = function (e) {
                     var event = window.event || e;
                     event.target = event.target ? event.target : event.srcElement;
-                    bindEvent(event.target, _vm, "dblclick", event);
+                    bindEvent(this, event.target, _vm, "dblclick", event);
 
                 }
                 newDom.onkeydown = function (e) {
                     var event = window.event || e;
                     event.target = event.target ? event.target : event.srcElement;
-                    bindEvent(event.target, _vm, "keydown", event);
+                    bindEvent(this, event.target, _vm, "keydown", event);
                 }
                 newDom.onkeyup = function (e) {
                     var event = window.event || e;
                     event.target = event.target ? event.target : event.srcElement;
-                    bindEvent(event.target, _vm, "keyup", event);
+                    bindEvent(this, event.target, _vm, "keyup", event);
                 }
                 if (isMobile()) {//移动
                     newDom.addEventListener("touchstart", function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "touchstart", event);
+                        bindEvent(this, event.target, _vm, "touchstart", event);
                     })
                     newDom.addEventListener("touchmove", function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "touchmove", event);
+                        bindEvent(this, event.target, _vm, "touchmove", event);
                     })
                     newDom.addEventListener("touchend", function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "touchend", event);
+                        bindEvent(this, event.target, _vm, "touchend", event);
 
                     })
                     newDom.addEventListener("touchcancel", function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "touchcancel", event);
+                        bindEvent(this, event.target, _vm, "touchcancel", event);
 
                     })
 
@@ -1746,88 +1771,89 @@
                     newDom.onmousedown = function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "mousedown", event);
+                        bindEvent(this, event.target, _vm, "mousedown", event);
 
                     }
                     newDom.onmouseup = function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "mouseup", event);
+                        bindEvent(this, event.target, _vm, "mouseup", event);
                     }
                     newDom.onmousemove = function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "mousemove", event);
+                        bindEvent(this, event.target, _vm, "mousemove", event);
                     }
                     newDom.onmouseout = function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "mouseout", event);
-                        bindEvent(event.target, _vm, "mouseleave", event, true);
+                        bindEvent(this, event.target, _vm, "mouseout", event);
+                        bindEvent(this, event.target, _vm, "mouseleave", event, true);
                     }
                     newDom.onmouseover = function (e) {
                         var event = window.event || e;
                         event.target = event.target ? event.target : event.srcElement;
-                        bindEvent(event.target, _vm, "mouseover", event);
-                        bindEvent(event.target, _vm, "mouseenter", event, true);
+                        bindEvent(this, event.target, _vm, "mouseover", event);
+                        bindEvent(this, event.target, _vm, "mouseenter", event, true);
                     }
                 }
-            }
-            function bindEvent(_this, vmName, vType, event, rec) {
-                function getTarget(_this) {
-                    if (_this == null || _this == undefined) {
-                        return;//ie修复
-                    }
-                    var veType
-                    try {
-                        veType = _this.getAttribute(PREFIX + "-vetype");
-                    } catch (e) {
-                        return;//ie问题修复
-                    }
-                    if ((veType !== undefined) && (veType !== null)) {
-                        if (veType === vType || qc.ve.hasOwnProperty(veType)) {
-                            var path = _this.getAttribute(PREFIX + "-path");
-                            var veName = _this.getAttribute(PREFIX + "-vename");
-                            if (qc.vms[vmName].$ve.hasOwnProperty(veName) && typeof qc.vms[vmName].$ve[veName] === "function") {
-                                path = path.split(".");
-                                var vm = qc.vms[path[0]];
-                                for (var i = 1; i < path.length; i++) {
-                                    var s = path[i];
-                                    if (s.indexOf("[") > -1) {
-                                        var index = s.substr(s.indexOf("["));
-                                        index = index.substr(1, index.length - 2);
-                                        index = index * 1;
-                                        s = s.substr(0, s.indexOf("["));
-                                        vm = vm[s][index];
-                                    } else {
-                                        vm = vm[s];
-                                    }
-                                }
-                                var run = (function (vmName, veName, _this, vm, event) {
-                                    return function (action) {
-                                        qc.vms[vmName].$ve[veName].call(_this, vm, event, action);//action 用于扩展组合事件
-                                    }
-                                })(vmName, veName, _this, vm, event);
-                                if (veType === vType) {
-                                    run();
-                                } else {
-                                    var extend = qc.ve[veType];
-                                    if (extend.hasOwnProperty(vType) && typeof extend[vType] === "function") {
-                                        extend[vType].call(extend, run, event, _this);
-                                    }
-                                }
-                            }
-                        } else if (_this !== newDom && _this.parentNode !== newDom && rec !== true) {
-                            getTarget(_this.parentNode)
-                        }
-                    } else if (_this !== newDom && _this.parentNode !== newDom) {
-                        getTarget(_this.parentNode)
-                    }
-                }
-
-                getTarget(_this);
             }
         }
+        function bindEvent(newDom, _this, vmName, vType, event, rec) {
+            function getTarget(_this) {
+                if (_this == null || _this == undefined) {
+                    return;//ie修复
+                }
+                var veType
+                try {
+                    veType = _this.getAttribute(PREFIX + "-vetype");
+                } catch (e) {
+                    return;//ie问题修复
+                }
+                if ((veType !== undefined) && (veType !== null)) {
+                    if (veType === vType || qc.ve.hasOwnProperty(veType)) {
+                        var path = _this.getAttribute(PREFIX + "-path");
+                        var veName = _this.getAttribute(PREFIX + "-vename");
+                        if (qc.vms[vmName].$ve.hasOwnProperty(veName) && typeof qc.vms[vmName].$ve[veName] === "function") {
+                            path = path.split(".");
+                            var vm = qc.vms[path[0]];
+                            for (var i = 1; i < path.length; i++) {
+                                var s = path[i];
+                                if (s.indexOf("[") > -1) {
+                                    var index = s.substr(s.indexOf("["));
+                                    index = index.substr(1, index.length - 2);
+                                    index = index * 1;
+                                    s = s.substr(0, s.indexOf("["));
+                                    vm = vm[s][index];
+                                } else {
+                                    vm = vm[s];
+                                }
+                            }
+                            var run = (function (vmName, veName, _this, vm, event) {
+                                return function (action) {
+                                    qc.vms[vmName].$ve[veName].call(_this, vm, event, action);//action 用于扩展组合事件
+                                }
+                            })(vmName, veName, _this, vm, event);
+                            if (veType === vType) {
+                                run();
+                            } else {
+                                var extend = qc.ve[veType];
+                                if (extend.hasOwnProperty(vType) && typeof extend[vType] === "function") {
+                                    extend[vType].call(extend, run, event, _this);
+                                }
+                            }
+                        }
+                    } else if (_this !== newDom && _this.parentNode !== newDom && rec !== true) {
+                        getTarget(_this.parentNode)
+                    }
+                } else if (_this !== newDom && _this.parentNode !== newDom) {
+                    getTarget(_this.parentNode)
+                }
+            }
+
+            getTarget(_this);
+        }
+
         //创建控件
         qc.createWidget = function (name, implement) {
             if (!qc.hasOwnProperty("widget")) {
