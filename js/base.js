02785bc7c86eae491c3cf15b4f32ca28ccf8ea8e
zhangyan <time-go@163.com>
2018-09-07 23:41:08 +0800

    update website

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 function left(page) {
-    $("#learn-sider").html(require("./templete/left.html!text"));
+    $("#learn-sider").html(need("./templete/left.html!text"));
     $("[data-index =\"" + page + "\"]").addClass("active");
     $("#learn-sider").on("click", "li", function () {
         window.location.href = $(this).attr("data-index") + ".html";
