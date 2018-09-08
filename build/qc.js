baad0e8f469202c7e297d4e5a6f12c0a0cad51cf
zhangyan <time-go@163.com>
2018-09-08 09:55:40 +0800

    ##commonjs

--- build/qc.js
+++ build/qc.js
@@ -2177,7 +2177,25 @@
  * commonjs.js
  */
 
-;(function () {
+; (function () {
+    var get = function (url) {
+        // XMLHttpRequest对象用于在后台与服务器交换数据
+        url = url + "?r=" + (new Date() - 1);
+        var retStr = "";
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.onreadystatechange = function () {
+            // readyState == 4说明请求已完成
+            if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) {
+                // 从服务器获得数据 
+                retStr = xhr.responseText;
+            } else {
+                window.console && console.log(url + "加载失败");
+            }
+        };
+        xhr.send();
+        return retStr;
+    }
     //---------------commonjs规范----------------//;
     window.$need = function (parent, path) {
         var _model;
@@ -2198,17 +2216,7 @@
                 _path = _path + ".js";
             }
         }
-        $.ajax({
-            "url": _path + "?r=" + (new Date() - 1),
-            async: false,
-            "error": function () {
-                window.console && console.log(_path + "加载失败");
-            },
-            "success": function (data) {
-                _model = data;
-            }
-        });
-
+        _model = get(_path);
         if (_type == "js") { //js预编译
             var _script = "(function(out){\n";
             _script += "var $parent = \"" + _basePath + "\";\n";
@@ -2219,13 +2227,13 @@
         }
         return _model;
     }
-    var getPath = function(){
-        var jsPath = document.currentScript ? document.currentScript.src : function(){
+    var getPath = function () {
+        var jsPath = document.currentScript ? document.currentScript.src : function () {
             var js = document.scripts
-                ,last = js.length - 1
-                ,src;
-            for(var i = last; i > 0; i--){
-                if(js[i].readyState === 'interactive'){
+                , last = js.length - 1
+                , src;
+            for (var i = last; i > 0; i--) {
+                if (js[i].readyState === 'interactive') {
                     src = js[i].src;
                     break;
                 }
@@ -2235,7 +2243,6 @@
         return jsPath.substring(0, jsPath.lastIndexOf('/') + 1);
     }
     window.need = function (path) {
-        console.log(path)
         return $need(getPath(), path);
     };
 })();;
